a1e20e2a09a453c39a04a6254c640bf546c46723
Matvey <klishnev2018@gmail.com>
2025-10-15 21:14:02 +0500

    10/15/2025

--- script.py
+++ script.py
@@ -1,22 +1,16 @@
 import requests
 import json
+import os
 
 url = "https://api.openai.com/v1/chat/completions"
-
 headers = {
     "Content-Type": "application/json",
-    "Authorization": "Bearer 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"
+    "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}"
 }
-
 data = {
     "model": "gpt-4o-mini",
-    "messages": [
-        {"role": "user", "content": "Объясни, что такое токен в LLM"}
-    ]
+    "messages": [{"role": "user", "content": "Объясни, что такое токен в LLM"}]
 }
 
-response = requests.post(url, headers = headers, json = data)
-
-result = response.json()
-
-print(result["choices"][0]["message"]["content"])
+response = requests.post(url, headers=headers, json=data)
+print(response.json()["choices"][0]["message"]["content"])
